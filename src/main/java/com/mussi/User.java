1db2c8eda1f18b0018d2214a79e4722fbafcf0f5
Tiago Mussi <tiago.mussi@eproseed.com>
2018-06-14 08:43:28 +0200

    Arquillian, eclipselink and derby setup.

--- src/main/java/com/mussi/User.java
+++ src/main/java/com/mussi/User.java
@@ -1,7 +1,20 @@
 package com.mussi;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private BigDecimal id;
     private String name;
     private String email;
     private String password;
